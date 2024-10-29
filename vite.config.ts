056cad18960770a7923af65d9f90c0c9315e3972
anthonyisensee <60198052+anthonyisensee@users.noreply.github.com>
2024-10-29 19:21:24 -0400

    chore: add browser router

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow access from WSL
+    port: 3000,
+    watch: {
+      usePolling: true, // Use polling to detect file changes
+    },
+  },
 })
