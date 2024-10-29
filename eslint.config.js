86df3958bd16e9ef91d28d0fec75fe50118ce3d0
anthonyisensee <60198052+anthonyisensee@users.noreply.github.com>
2024-10-29 07:51:09 -0400

    chore: rebuilds configuration

--- eslint.config.js
+++ eslint.config.js
@@ -7,15 +7,11 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked, ...tseslint.configs.stylisticTypeChecked],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -27,18 +23,6 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-    },
-    // Set the react version
-    settings: { react: { version: '18.3' } },
-      plugins: {
-      // Add the react plugin
-      react,
-    },
-      rules: {
-      // other rules...
-      // Enable its recommended rules
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
     },
   },
 )
