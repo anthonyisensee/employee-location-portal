7e4726f0c971c3a22bcf231f43d7ef363f57e63c
anthonyisensee <60198052+anthonyisensee@users.noreply.github.com>
2024-10-29 22:00:43 -0400

    feat: imports mui and begins basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Container, createTheme, CssBaseline, ThemeProvider, Typography, useMediaQuery } from '@mui/material'
+
+const darkTheme = createTheme({
+  colorSchemes: {
+    dark: true,
+  }
+})
+
+const lightTheme = createTheme({
+  colorSchemes: {
+    light: true,
+  }
+})
 
 function App() {
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
   return (
-    <BrowserRouter>
-      <p>Test</p>
-    </BrowserRouter>
+    <ThemeProvider theme={prefersDarkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Container>
+          <Typography variant="h4" component="h1">Employee Location Portal</Typography>
+          <Typography variant="body1">Welcome to the employee location portal application!</Typography>
+        </Container>      
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
